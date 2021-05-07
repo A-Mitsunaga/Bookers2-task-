950b0de29be25063427268a5fc3d5b7e897b4dcb
A-Mitsunaga <a56st70410@icloud.com>
2021-05-07 11:02:42 +0000

    [Add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = Book.all
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def book_params
+    params.require(:user).permit(:name, :profile_image)
+    # 追加あるかも
+  end
+
+
 end
