d608195b85864e7c5dbbf257edda8877e60aadfb
A-Mitsunaga <a56st70410@icloud.com>
2021-05-09 09:44:31 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,12 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(id: current_user)
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.find_by(id: params[:id])
+    @books = Book.all
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = Book.all
+    @books = @user.books
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    @users = User.all
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = Book.new(book_param)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to books_path(@user.id)
   end
 
   def edit
@@ -21,13 +23,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+   @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
 
   private
-  def book_params
+  def user_params
     params.require(:user).permit(:name, :profile_image)
     # 追加あるかも
   end
