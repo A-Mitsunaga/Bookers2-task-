eab15745cf05f262a8f44092d15811e522fc4c6c
A-Mitsunaga <a56st70410@icloud.com>
2021-05-16 05:32:42 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :homes, only: [:index]
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index]
-  post 'books' => 'books#create'
-  get '/books/:id' => 'books#show'
-  post 'users/:id' => 'users#show'
-  delete '/books/:id' => 'books#destroy'
-  post '/books/:id/edit' => 'books#edit'
-  put '/books/:id' => 'books#update'
+  get 'home/about' => 'homes#index'
 
 end
